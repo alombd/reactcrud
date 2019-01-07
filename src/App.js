bb56625201545662eaf2b9677a660dd2984f2395
MD: ALOMGIR HOSSEN <alombd.bd@gmail.com>
2019-01-07 17:40:40 +0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,45 @@
+import React, { Component } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { BrowserRouter as Router, Switch, Route, Link,state,axios } from 'react-router-dom';
+import logo from './logo.svg';
+import './App.css';
+
+import Create from './components/create.component.js';
+import Edit from './components/edit.component.js';
+import Index from './components/index.component.js';
+
+class App extends Component {
+  render() {
+    return (
+      <Router>
+        <div className="container">
+          <nav className="navbar navbar-expand-lg navbar-light bg-light">
+            <Link to={'/'} className="navbar-brand">React CRUD Example</Link>
+            <div className="collapse navbar-collapse" id="navbarSupportedContent">
+              <ul className="navbar-nav mr-auto">
+              <li className="nav-item">
+                  <Link to={'/'} className="nav-link">Home</Link>
+                </li>
+                <li className="nav-item">
+                  <Link to={'/create'} className="nav-link">Create</Link>
+                </li>
+                <li className="nav-item">
+                  <Link to={'/index'} className="nav-link">Index</Link>
+                </li>
+              </ul>
+            </div>
+          </nav> <br/>
+          <h2>Welcome to React CRUD Tutorial</h2> <br/>
+          <Switch>
+              <Route exact path='/create' component={ Create } />
+              <Route path='/edit/:id' component={ Edit } />
+              <Route path='/index' component={ Index } />
+          </Switch>
+        </div>
+      </Router>
+    );
+  }
+}
+
+
+export default App;
---
+++ src/components/index.component.js
@@ -0,0 +1,52 @@
+// index.component.js
+
+import React, { Component } from 'react';
+import axios from 'axios';
+import TableRow from './TableRow';
+import { Link } from 'react-router-dom';
+
+export default class Index extends Component {
+
+  constructor(props) {
+      super(props);
+      this.state = {business: []};
+    }
+    componentDidMount(){
+      axios.get('http://localhost:4000/business')
+        .then(response => {
+          this.setState({ business: response.data });
+        })
+        .catch(function (error) {
+          console.log(error);
+        })
+    }
+    tabRow(){
+      return this.state.business.map(function(object, i){
+          return <TableRow obj={object} key={i} />;
+      });
+    }
+
+    render() {
+      return (
+        <div>
+          <h3 align="center">Business List</h3>
+          <table className="table table-striped" style={{ marginTop: 20 }}>
+            <thead>
+            <tr align="center">
+                <Link to={'/create'} className="btn btn-primary">Create</Link>
+            </tr>
+              <tr>
+                <th>Person</th>
+                <th>Business</th>
+                <th>GST Number</th>
+                <th colSpan="2">Action</th>
+              </tr>
+            </thead>
+            <tbody>
+              { this.tabRow() }
+            </tbody>
+          </table>
+        </div>
+      );
+    }
+  }
